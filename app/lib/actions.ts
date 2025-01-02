b51939741d2db388406a67af72edd28a3a6d7c04
Boamah-Powers <kb4242@nyu.edu>
2025-01-02 20:06:29 +0000

    fixed type error in actions.ts

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -32,9 +32,10 @@ export async function createInvoice(formData: FormData) {
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
 	} catch (error) {
-    return {
-      message: 'Database Error: Failed to Create Invoice.',
-    };
+    console.log('Database Error: Failed to Create Invoice.')
+    // return {
+    //   message: 'Database Error: Failed to Create Invoice.',
+    // };
   }
 
 	revalidatePath("/dashboard/invoices");
@@ -57,9 +58,10 @@ export async function updateInvoice(id: string, formData: FormData) {
       WHERE id = ${id}
     `;
   } catch (error) {
-    return {
-      message: 'Database Error: Failed to Update Invoice.',
-    };
+    console.log('Database Error: Failed to Update Invoice.')
+    // return {
+    //   message: 'Database Error: Failed to Update Invoice.',
+    // };
   }
 
 
